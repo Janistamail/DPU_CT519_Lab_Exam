a28d21b2343bbe7e851e822992fc82268665d5eb
Janistamail <janista.shrw@gmail.com>
2022-10-05 14:01:05 +0700

    done

--- App_A/backend/pool.js
+++ App_A/backend/pool.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2/promise");
 
 var pool = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
+  host: "db",
   user: "root",
   password: "root",
   database: "0030_Lab_Exam",
